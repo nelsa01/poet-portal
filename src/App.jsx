36aa970890abe7bb1a1174714d8e47d867ddcb7f
felix <niyomugabofelix@gmail.com>
2022-08-11 08:42:39 +0200

    writepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 
 import './App.css';
+import Right from './ui/components/WritePage/Right/Right';
+import Write from './ui/components/WritePage/Write';
 // import HomePage from './ui/components/HomePage/HomePage';
-import Middle from './ui/LandingPage/Middle/Middle';
+// import Middle from './ui/LandingPage/Middle/Middle';
 import Sidebar from './ui/LandingPage/Navigation/Sidebar';
-import Right from './ui/LandingPage/Right/Right';
+// import Right from './ui/LandingPage/Right/Right';
 
 function App() {
   return (
     <div className="App">
-      
-      
-      
+
+      <Write />   
+      <Right />   
       {/* <HomePage /> */}
      <div id="sidebarr"><Sidebar /></div>
-     <div id='middlebarr'><Middle /></div>
-     <div id='rightbar'><Right /></div>
+     {/* <div id='middlebarr'><Middle /></div> */}
+     {/* <div id='rightbar'><Right /></div> */}
      
      </div>
   );
---
+++ src/ui/components/WritePage/Write.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import "./Write.css"
+
+const Write = () => {
+  return (
+    <div className='write'>
+        <h3>Write</h3><br />
+        <h6>Poem Title*</h6><br />
+        <div className='input_title'>
+            <input type="text"/>
+    </div><br />
+    <div className='about'>
+        <h6>About This Poem*</h6><br />
+        <input type="text" />
+
+    </div><br />
+
+    <div className='poem'>
+        <h6>Poem*</h6><br />
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+
+    </div><br />
+    <div className='select'>
+        <h6>Genre*</h6><br />
+        <select id="select_option">
+    <option value="volvo">Select genre</option>
+    <option value="volvo">Military</option>
+    <option value="saab">Spy Fiction</option>
+    <option value="opel">Comedy</option>
+    <option value="audi">Parodies</option>
+  </select><br /><br />
+  <a href="">suggest a new genre</a>
+
+    </div>
+    <div className='select'><br />
+        <h6>Poems Form*</h6><br />
+        <select id="select_option">
+    <option value="volvo">Select Form</option>
+    <option value="volvo">Military</option>
+    <option value="saab">Spy Fiction</option>
+    <option value="opel">Comedy</option>
+    <option value="audi">Parodies</option>
+  </select>
+  </div><br />
+  <div className='date'>
+    <h6>Date Written*</h6>
+  <input type="date" id="birthday" name="birthday"></input>
+    </div><br />
+    <div className='public'>
+        <button><a href=""> Publish</a></button>
+    </div>
+    </div>
+  )
+}
+
+export default Write
